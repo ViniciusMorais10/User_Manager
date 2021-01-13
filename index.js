c0af96cfd0019e706aaea6fcb55964d274e93b3d
Vinicius <viniciuz.lol5@gmail.com>
2021-01-13 10:58:27 -0300

    Projeto Hcode

---
+++ index.js
@@ -0,0 +1,10 @@
+//ConhecimentoLivre
+var name = document.querySelector("#exampleInputName");
+var genderM = document.querySelector("#exampleInputGenderM");
+var genderF = document.querySelector("#exampleInputGenderF");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var mail = document.querySelector("#exampleInputEmail1");
+var password = document.querySelector("#exampleInputPassword1");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#admin");
